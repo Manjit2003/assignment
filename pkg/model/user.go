e3756456345dafd807761830b02bbb03eb037f81
Manjeet Pardeshi <manjitpardeshi91991@gmail.com>
2024-06-26 00:30:44 +0530

    added some basic queris

--- pkg/model/user.go
+++ pkg/model/user.go
@@ -1,7 +1,11 @@
 package model
 
+import "time"
+
 type User struct {
-	ID             string `json:"id"`
-	Username       string `json:"username"`
-	HashedPassword string `json:"-"` // omit from the responses
+	ID             string    `json:"id"`
+	Username       string    `json:"username"`
+	HashedPassword string    `json:"-"` // omit from the responses
+	Created        time.Time `json:"created"`
+	Updated        time.Time `json:"updated"`
 }
--- pkg/service/user/user.go
+++ pkg/service/user/user.go
@@ -1,11 +1,36 @@
 package user_service
 
-import "github.com/Manjit2003/samespace/pkg/db"
+import (
+	"github.com/Manjit2003/samespace/pkg/db"
+	"github.com/Manjit2003/samespace/pkg/model"
+	"github.com/gocql/gocql"
+)
 
 var session = db.ScyllaSession
 
-func AddUser(username, hashed_password string) (string, error) {
-	session.Query("")
+const (
+	queryGetUser = `SELECT id, username, created, updated FROM your_keyspace_name.users
+              WHERE username = ? AND hashed_password = ?;`
+	queryAddUser = `INSERT INTO users (id, username, hashed_password, created, updated)
+              VALUES (uuid(), ?, ?, toTimestamp(now()), toTimestamp(now()))`
+)
 
-	return "", nil
+func AddUser(username, hashedPassword string) error {
+	return session.Query(queryAddUser, username, hashedPassword).Exec()
+}
+
+func GetUser(username, hashedPassword string) (*model.User, error) {
+	var user model.User
+	if err := session.Query(queryGetUser, username, hashedPassword).Consistency(gocql.One).Scan(
+		&user.ID,
+		&user.Username,
+		&user.Created,
+		&user.Updated,
+	); err != nil {
+		if err == gocql.ErrNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
 }
